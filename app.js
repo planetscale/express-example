036e65b6dc375f0bd6671f7b04449445e52ae9f4
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2021-05-14 16:03:47 -0400

    Get port for Heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const mysql = require('mysql')
 const connection = mysql.createConnection(process.env.DATABASE_URL)
