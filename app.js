9379a31d7e22ad0c2467ff33f267539076d3b75b
James Q Quick <james.q.quick@gmail.com>
2022-01-04 12:17:05 -0600

    Adding database name environment variable and reference in the docs

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000
 const mysql = require('mysql2')
 const connection = mysql.createConnection({
   host : process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
   user : process.env.DATABASE_USERNAME,
   password : process.env.DATABASE_PASSWORD,
   ssl : {
