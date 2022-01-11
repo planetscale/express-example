d0d89513adba7b89b5b44fd091e6c9320ae8b0aa
Holly Guevara <holly@planetscale.com>
2022-01-11 10:10:37 -0700

    option to connect with client certs

--- app.js
+++ app.js
@@ -4,15 +4,33 @@ const app = express()
 const port = process.env.PORT || 3000
 
 const mysql = require('mysql2')
-const connection = mysql.createConnection({
-  host : process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  user : process.env.DATABASE_USERNAME,
-  password : process.env.DATABASE_PASSWORD,
-  ssl : {
-      rejectUnauthorized: true
-  }
-});
+let connection = {};
+
+/*
+  ---- Note: You only need one connection object in the if/else statement ----
+
+  OPTION 1:
+    Use if you're using connection strings in your app (see docs)
+    https://docs.planetscale.com/tutorials/connect-nodejs-app#using-a-connection-string
+
+  OPTION 2:
+    Use if you're connecting with client certificates via CLI (see docs)
+    https://docs.planetscale.com/tutorials/connect-nodejs-app#using-client-certificates-with-the-cli
+*/
+
+if (process.env.DATABASE_HOST) {
+  connection = mysql.createConnection({
+    host : process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    user : process.env.DATABASE_USERNAME,
+    password : process.env.DATABASE_PASSWORD,
+    ssl : {
+        rejectUnauthorized: true
+    }
+  });
+} else {
+  connection = mysql.createConnection(process.env.DATABASE_URL)
+}
 
 connection.connect()
 
